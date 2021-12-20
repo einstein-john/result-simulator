6fb3c8a81575bc0f4d79599e12eae50ac630909a
einstein-john <81680974+einstein-john@users.noreply.github.com>
2021-12-20 09:05:21 +0200

    Update result.cpp
    added some comments to make the code more readable

--- result.cpp
+++ result.cpp
@@ -18,14 +18,17 @@ int main(void){
     cout<<"Enter the  name of the second team: ";
     cin>>team_2;
     cout<<endl<< endl;
-    scrT1 = rand() % 10 + 1;//generates a random for team 1
-    scrT2 = rand() % 10 + 2;//generates a random for team 2
+    scrT1 = rand() % 10 + 1;//generates a random for team 1 (between 1 and 10)
+    scrT2 = rand() % 10 + 2;//generates a random for team 2 (between 1 and 10)
+   
     if (team_1.length() != 0 && team_2.length() != 0 ) {
         printResult(scrT1, scrT2, team_1, team_2);//print result
     }//checks if team name is empty
 
     return 0;
 }
+
+//takes the team names and gives them a random number for their scores
 void printResult(int scrT1, int scrT2,string team_1, string team_2) {
     cout << "===============[ THE RESULT OF THE MATCH  ]===============" << endl << endl;
     cout<<team_1<<"->"<<scrT1<<" <---> "<<team_2<<"->"<<scrT2<<endl;
